d1544f0bca35ca66e9e0c725f449d0d0bd9d00f7
r-cha <robert@akchandler.com>
2022-05-14 08:44:32 -0700

    Get non-singleton config working

--- db/base.go
+++ db/base.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"r-cha/goblog/config"
 	"r-cha/goblog/models"
 
 	"gorm.io/driver/postgres"
@@ -14,11 +15,10 @@ func Connect() {
 	var database *gorm.DB
 	var err error
 
-	// settings := config.GetSettings()
-	env := "local" // settings.environment
+	settings := config.NewSettings()
+	env := settings.ENVIRONMENT
 	if env == "prod" {
-		dsn := "connectionstring"
-		database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		database, err = gorm.Open(postgres.Open(settings.POSTGRES_DSN), &gorm.Config{})
 	} else if env == "local" {
 		database, err = gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 	}
